73a291a4955af9b98033992bae5f8b1cb70be1cc
Martin Zikmund <martin@zikmund.dev>
2020-08-09 16:57:35 +0200

    Vibration

---
+++ UnexpectedApisDemo/UnexpectedApisDemo.Shared/Views/VibrationPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Phone.Devices.Notification;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UnexpectedApisDemo.Shared.Views
+{
+    public sealed partial class VibrationPage : Page
+    {
+        public VibrationPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Vibrate_Tap(object sender, TappedRoutedEventArgs e)
+        {
+            var vibrationDevice = VibrationDevice.GetDefault();
+            if (vibrationDevice != null)
+            {
+                vibrationDevice.Vibrate(TimeSpan.FromMilliseconds(200));
+            }
+        }
+    }
+}
