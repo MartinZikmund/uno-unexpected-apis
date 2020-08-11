fc0df1a359b340088d6ed31d67ed33a81516c04b
Martin Zikmund <martin@zikmund.dev>
2020-08-11 22:03:34 +0200

    Set app title, reorder samples

--- UnexpectedApisDemo/UnexpectedApisDemo.Wasm/WasmScripts/AppManifest.js
+++ UnexpectedApisDemo/UnexpectedApisDemo.Wasm/WasmScripts/AppManifest.js
@@ -2,6 +2,6 @@
 
     splashScreenImage: "Assets/SplashScreen.scale-400.png",
     splashScreenColor: "#1F1F1F",
-    displayName: "UnexpectedApisDemo"
+    displayName: "Unexpected APIs in Uno Platform"
 
 }
