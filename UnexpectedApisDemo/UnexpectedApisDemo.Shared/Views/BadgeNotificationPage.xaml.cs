06c853593adbd7c4a4257f1b1b502c86102447aa
Martin Zikmund <martin@zikmund.dev>
2022-11-29 23:12:29 +0100

    Add Badge Notification sample

--- UnexpectedApisDemo/UnexpectedApisDemo.Shared/Views/BadgeNotificationPage.xaml.cs
+++ UnexpectedApisDemo/UnexpectedApisDemo.Shared/Views/BadgeNotificationPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Data.Xml.Dom;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,14 +19,28 @@ using Windows.UI.Xaml.Navigation;
 
 namespace UnexpectedApisDemo.Shared.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class BadgeNotification : Page
+    public sealed partial class BadgeNotificationPage : Page
     {
-        public BadgeNotification()
+        public BadgeNotificationPage()
         {
             this.InitializeComponent();
+        }
+
+        private void SetBadge_Click(object sender, RoutedEventArgs e)
+        {
+            var badgeXml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeNumber);
+            var badgeGlyphXml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeGlyph);
+            XmlElement badgeElement = badgeXml.SelectSingleNode("/badge") as XmlElement;
+            badgeElement.SetAttribute("value", BadgeNumberBox.Value.ToString());
+
+            var badgeNotification = new BadgeNotification(badgeXml);
+
+            BadgeUpdateManager.CreateBadgeUpdaterForApplication().Update(badgeNotification);
+        }
+
+        private void ClearBadge_Click(object sender, RoutedEventArgs e)
+        {
+            BadgeUpdateManager.CreateBadgeUpdaterForApplication().Clear();
         }
     }
 }
