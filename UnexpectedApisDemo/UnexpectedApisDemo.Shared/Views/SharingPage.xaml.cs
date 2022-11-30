a41de5a322aa319ed8e72afc9869a7f9ae7a2aa6
Martin Zikmund <martin@zikmund.dev>
2022-11-30 11:40:16 +0100

    Sharing demo

--- UnexpectedApisDemo/UnexpectedApisDemo.Shared/Views/SharingPage.xaml.cs
+++ UnexpectedApisDemo/UnexpectedApisDemo.Shared/Views/SharingPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +27,46 @@ namespace UnexpectedApisDemo.Shared.Views
         public SharingPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            DataTransferManager.GetForCurrentView().DataRequested += SharingPage_DataRequested;
+        }
+
+        private void Share_Click(object sender, RoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            DataTransferManager.GetForCurrentView().DataRequested -= SharingPage_DataRequested;
+        }
+
+        private void SharingPage_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var deferral = args.Request.GetDeferral();
+
+            args.Request.Data.ShareCompleted += Data_ShareCompleted;
+            args.Request.Data.ShareCanceled += Data_ShareCanceled;
+            args.Request.Data.Properties.Title = "Unexpected APIs in Uno Platform";
+            args.Request.Data.Properties.Description = "Link to the app";
+            args.Request.Data.SetWebLink(new Uri("https://cutt.ly/apis"));
+
+            deferral.Complete();
+        }
+
+        private void Data_ShareCanceled(DataPackage sender, object args)
+        {
+            sender.ShareCanceled -= Data_ShareCanceled;
+        }
+
+        private void Data_ShareCompleted(DataPackage sender, ShareCompletedEventArgs args)
+        {
+            sender.ShareCompleted -= Data_ShareCompleted;
         }
     }
 }
