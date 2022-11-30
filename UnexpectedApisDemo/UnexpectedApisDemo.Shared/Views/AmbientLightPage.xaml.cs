466a34afe8ad51cc51390551e0fdc97f0bb247a1
Martin Zikmund <martin@zikmund.dev>
2022-11-30 10:22:45 +0100

    Ambient light sensor sample

--- UnexpectedApisDemo/UnexpectedApisDemo.Shared/Views/AmbientLightPage.xaml.cs
+++ UnexpectedApisDemo/UnexpectedApisDemo.Shared/Views/AmbientLightPage.xaml.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Reactive.Disposables;
+using System.Threading.Tasks;
+using UnexpectedApisDemo.Shared.ViewModels;
+using Windows.Devices.Sensors;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
@@ -22,9 +19,127 @@ namespace UnexpectedApisDemo.Shared.Views
     /// </summary>
     public sealed partial class AmbientLightPage : Page
     {
+        private AmbientLightSensorViewModel _viewModel;
+
         public AmbientLightPage()
         {
             this.InitializeComponent();
+        }
+
+        public AmbientLightSensorViewModel ViewModel => _viewModel ?? (_viewModel = new AmbientLightSensorViewModel(Dispatcher));
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            ViewModel.Initialize();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            ViewModel.Disposables.Dispose();
+        }
+    }
+
+    public class AmbientLightSensorViewModel : ViewModelBase
+    {
+        private LightSensor _hinge;
+        private bool _readingChangedAttached;
+        private string _sensorStatus;
+        private string _lux;
+        private readonly CoreDispatcher _dispatcher;
+        private bool _noSensor = false;
+        private Brush _backgroundBrush = new SolidColorBrush(Colors.Transparent);
+        
+        public AmbientLightSensorViewModel(CoreDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        public bool NoSensor
+        {
+            get => _noSensor;
+            set
+            {
+                _noSensor = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public void Initialize()
+        {
+            _hinge = LightSensor.GetDefault();
+            RaisePropertyChanged(nameof(AmbientLightSensorAvailable));
+            if (_hinge == null)
+            {
+                NoSensor = true;
+                return;
+            }
+
+            Disposables.Add(Disposable.Create(() =>
+            {
+                if (_hinge != null)
+                {
+                    _hinge.ReadingChanged -= AmbientLightSensor_ReadingChanged;
+                }
+            }));
+            _hinge.ReadingChanged += AmbientLightSensor_ReadingChanged;
+        }
+
+        public bool AmbientLightSensorAvailable => _hinge != null;
+
+        public string SensorStatus
+        {
+            get => _sensorStatus;
+            private set
+            {
+                _sensorStatus = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool ReadingChangedAttached
+        {
+            get => _readingChangedAttached;
+            private set
+            {
+                _readingChangedAttached = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Lux
+        {
+            get => _lux;
+            private set
+            {
+                _lux = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Brush BackgroundBrush
+        {
+            get => _backgroundBrush;
+            set
+            {
+                _backgroundBrush = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private async void AmbientLightSensor_ReadingChanged(LightSensor sender, LightSensorReadingChangedEventArgs args)
+        {
+            await _dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Lux = string.Format("{0:0}", Math.Round(args.Reading.IlluminanceInLux));
+                var adjustedLux = Math.Max(Math.Min(args.Reading.IlluminanceInLux, 1000), 0);
+                var ratio = adjustedLux / 1000;
+
+                BackgroundBrush = new SolidColorBrush(Color.FromArgb(255, (byte)(ratio * 255), (byte)(ratio * 255), (byte)(ratio * 255)));
+            });
         }
     }
 }
