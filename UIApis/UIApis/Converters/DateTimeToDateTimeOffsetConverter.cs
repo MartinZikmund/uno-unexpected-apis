ac3aaef065b0d1d7c052972940d7b8ea0f3e7715
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2025-05-13 13:38:26 +0200

    feat: Add WCT `DataGrid` sample

---
+++ UIApis/UIApis/Converters/DateTimeToDateTimeOffsetConverter.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace UIApis.Converters;
+
+public class DateTimeToDateTimeOffsetConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        DateTime date = (DateTime)value;
+        return new DateTimeOffset(date);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        DateTimeOffset dateTimeOffset = (DateTimeOffset)value;
+        return dateTimeOffset.DateTime;
+    }
+}
