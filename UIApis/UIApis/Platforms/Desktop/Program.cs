0ccd6ab6d2827dc6804eff04d12a0f03f4781709
Martin Zikmund <martin@zikmund.dev>
2025-05-13 15:03:21 +0200

    feat: Add SKCanvasElement and GLCanvasElement

--- UIApis/UIApis/Platforms/Desktop/Program.cs
+++ UIApis/UIApis/Platforms/Desktop/Program.cs
@@ -1,14 +1,21 @@
 using UIApis;
 using Uno.UI.Hosting;
 
-App.InitializeLogging();
+internal class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        App.InitializeLogging();
 
-var host = UnoPlatformHostBuilder.Create()
-    .App(() => new App())
-    .UseX11()
-    .UseLinuxFrameBuffer()
-    .UseMacOS()
-    .UseWin32()
-    .Build();
+        var host = UnoPlatformHostBuilder.Create()
+            .App(() => new App())
+            .UseX11()
+            .UseLinuxFrameBuffer()
+            .UseMacOS()
+            .UseWin32()
+            .Build();
 
-host.Run();
+        host.Run();
+    }
+}
